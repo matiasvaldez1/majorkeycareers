210278167f7e3051db66946f3a4276b4c7571d0d
Matias Valdez <matiasvaldez8184@gmail.com>
2022-04-18 13:32:40 -0300

    Modal added

--- components/Cardjob.js
+++ components/Cardjob.js
@@ -1,19 +1,31 @@
-import React from 'react'
+import React,{useState} from 'react'
 import Link from 'next/link'
+import ModalCard from './ModalCard'
 
 export default function Cardjob({type,description,title,isOpen,isClosed,category,id}) {
     const descrip = description.replace(/<\/?[^>]+>/gi, '')
+    const [showModal,setShowModal] = useState(false)
     return (
+        <>
+        {showModal ? <ModalCard 
+        setShowModal={setShowModal}
+        description={descrip}
+        title={title}
+        category={category}
+        id={id}
+        type={type}
+        /> : null}
         <div className='bg-gray-100 my-12 text-center select-none space-y-3 rounded-md hover:bg-gray-100/50'>
             <h1 className='text-2xl font-semibold'>{title}</h1>
             <p className='text-sm'>{category}</p>
             <p>{type}</p>
             <p className='w-10/12 m-auto'>{descrip.slice(0,100)}-</p>
             <div className='flex justify-center'>
-                <a className='text-tertiary font-medium hover:opacity-60 cursor-pointer' href={`https://tradehelm.com/wp-content/plugins/bullhorn-oscp/#/jobs/${id}`} target="_blank" rel="noreferrer" >
+                <button onClick={() => setShowModal(!showModal)} className='text-tertiary font-medium hover:opacity-60 cursor-pointer' >
                 View detail
-                </a>
+                </button>
             </div>
         </div>
+        </>
     )
 }
